e5896e301c10f7297b249967d29298e6bd87a41b
mayank-ramnani <mayankr99@gmail.com>
2024-04-03 19:13:25 -0400

    bulb improvements

--- bulb.sh
+++ bulb.sh
@@ -1,13 +1,22 @@
 HOST=192.168.1.170
 
-# kasa --type bulb --host $HOST off
-kasa --type bulb --host $HOST on
-while true
-do
-    for hue in $(seq 0 15 360)
+if [ $1 == "off" ]; then
+    echo "turning bulb off"
+    kasa --type bulb --host $HOST off
+    exit 0
+elif [ $1 == "on" ]; then
+    echo "turning bulb on"
+    kasa --type bulb --host $HOST on
+    exit 0
+else
+    while true
     do
-        kasa --type bulb --host $HOST hsv $hue $(($RANDOM % 60 + 40)) 100 --transition 2000
-        # echo "Hue: $hue"
-        sleep 0.5
+        for hue in $(seq 0 15 360)
+        do
+            kasa --type bulb --host $HOST hsv $hue $(($RANDOM % 60 + 40)) 100 --transition 2000
+            # echo "Hue: $hue"
+            sleep 0.5
+        done
     done
-done
+fi
+
